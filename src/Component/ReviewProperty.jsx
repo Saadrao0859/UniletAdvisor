8f690e32a56a46b95406436bb2fbdcd3d70622fe
Saadrao0859 <saadshabirmailsi123@gmail.com>
2025-04-27 17:10:00 +0500

    responsive Review a Property Page

--- src/Component/ReviewProperty.jsx
+++ src/Component/ReviewProperty.jsx
@@ -12,7 +12,7 @@ import location from "../assets/images/ReviewsRecent/arrow.svg";
 
 const ReviewProperty = () => {
   return (
-    <div className="container">
+    <div className="container-Review-property">
       <div className="image-fst">
         <img src={fstimg} alt="" />
       </div>
@@ -31,7 +31,7 @@ const ReviewProperty = () => {
       <div className="recent-review">
         <h2>Recently Reviewed</h2>
         <p>More then 10000 clients who are happy with Us. Are You Next One?</p>
-        <div className="house-box house-box-lower">
+        <div className="house-box ">
           <div className="image-boxes">
             <div className="image-show">
               <img src={image9} alt="" />
